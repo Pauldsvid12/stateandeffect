ef9e9f86c6320fb77bff4f36140f142523f7dafb
pjuelamatute <pjuelamatute@gmail.com>
2025-10-12 20:04:44 +0000

    🧭 chore(layout): configurar estructura principal de navegación con expo-router - Se modifico el _layout.tsx para definir la navegación principal. - Integración con expo-router mediante Stack para manejar las pantallas: index, login, register - Se aplicó un estilo global oscuro.
    Oculta los encabezados (headerShown: false) para una experiencia más inmersiva.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,19 @@
-import { Stack } from "expo-router";
+// app/_layout.tsx
+import { Stack } from 'expo-router';
+import '../global.css';
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false,
+        contentStyle: { backgroundColor: '#191414' },
+      }}
+    >
+      <Stack.Screen name="index" />
+      <Stack.Screen name="auth/login" />
+      <Stack.Screen name="auth/register" />
+      <Stack.Screen name="home" />
+    </Stack>
+  );
 }
